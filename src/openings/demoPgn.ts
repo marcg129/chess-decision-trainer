b205e9ee21dca48f4698facddded49240035b627
Marc G <38407880+marcg129@users.noreply.github.com>
2026-09-17 15:30:23 -0400

    feat: add built in opening demo pgn

---
+++ src/openings/demoPgn.ts
@@ -0,0 +1,10 @@
+export const DEMO_REPERTOIRE_ID = '00000000-0000-4000-8000-000000000003';
+
+export const DEMO_PGN = `[Event "Italian Game Demo"]
+[White "Learner"]
+[Black "Trainer"]
+[Result "*"]
+
+1. e4 {Claim central space.} e5 (1... c5 2. Nf3 d6)
+2. Nf3 Nc6 3. Bc4 {Develop toward f7.} Nf6 (3... Bc5 4. c3)
+4. d3 Bc5 5. O-O d6 *`;
